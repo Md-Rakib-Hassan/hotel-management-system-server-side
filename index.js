1e484d8f949739b75a0e8127f7d14f6e3f538a24
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2023-11-08 01:28:27 +0600

    reviews api complete

--- index.js
+++ index.js
@@ -74,6 +74,17 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/api/v1/reviews',async(req, res) => {
+
+      const coursor=dataBase.collection('Reviews').find();
+      const result= await coursor.toArray();
+      res.send(result);
+    })
+
+
+
+
+
     app.get('/api/v1/room/:roomId',async(req, res) => {
       const result= await dataBase.collection('Hotel details').findOne({_id: new ObjectId(req.params.roomId)});
       res.send(result);
